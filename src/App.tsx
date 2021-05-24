a70839da8c010a857cb6275af5233b034a9d7b21
DmitriBelski <51079926+DmitriBelski@users.noreply.github.com>
2021-05-24 19:11:56 +0300

    Add Open/Close behavior to Select component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import Title from './components/title/Title';
 import Placeholder from './components/placeholder/Placeholder';
 import Card from './components/card/Card';
@@ -7,6 +7,31 @@ import Select from './components/select/Select';
 import Selectdate from './components/selectdate/Selectdate';
 
 function App(): JSX.Element {
+  // const key = '40e04da9c5aabaa94f2c9c89aeeb4578';
+
+  const [cityNames, setCityNames] = useState<string[]>([]);
+  const [dailyCity, setDailyCity] = useState<string>('');
+
+  const cities = useMemo(() => [
+    { name: 'Самара', lat: 53.195873, lon: 50.100193 },
+    { name: 'Тольятти', lat: 53.507836, lon: 49.420393 },
+    { name: 'Саратов', lat: 51.533557, lon: 46.034257 },
+    { name: 'Казань', lat: 55.796127, lon: 49.106405 },
+    { name: 'Краснодар', lat: 45.035470, lon: 38.975313 },
+    { name: 'Каменка', lat: 57.38232, lon: 41.79643 },
+  ],
+  []);
+
+  useEffect(() => {
+    const getCityNames = () => {
+      const citiesArray: string[] = [] as string[];
+      cities.map((item) => citiesArray.push(item.name));
+      return citiesArray;
+    };
+    const citiesArray: string[] = getCityNames();
+    setCityNames(citiesArray);
+  }, [cities]);
+
   return (
     <div className="wrapper">
       <Title />
@@ -15,7 +40,7 @@ function App(): JSX.Element {
         <div className="container container-top">
           <h1 className="h1-font" style={{ marginBottom: '32px' }}>7 Days Forecast</h1>
           <div style={{ marginBottom: '54px' }}>
-            <Select />
+            <Select initValue="Select city" items={cityNames} setter={setDailyCity} />
           </div>
           {/* <Placeholder /> */}
           <Slider />
@@ -27,7 +52,7 @@ function App(): JSX.Element {
             Forecast for a Date in the Past
           </h1>
           <div className="selectwrapper">
-            <Select />
+            <Select initValue="Select city" items={cityNames} setter={setDailyCity} />
             <Selectdate />
           </div>
           <Placeholder />
--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Scrollpicker from '../scrollpicker/Scrollpicker';
 
-function Select(): JSX.Element {
+type SelectProps = {
+  initValue: string,
+  items: string[],
+  setter(city: string): void
+}
+
+function Select({ initValue, items, setter }: SelectProps): JSX.Element {
+  const [value, setValue] = useState<string>('');
+  const [opened, setOpened] = useState<boolean>(false);
+  const [hovered, setHovered] = useState<boolean>(false);
+
+  useEffect(() => {
+    setValue(initValue);
+  }, [initValue]);
+
+  useEffect(() => {
+    if (value !== initValue) {
+      setOpened((prev) => !prev);
+      setter(value);
+    }
+  }, [initValue, value, setter]);
+
+  function HoverHandler() {
+    return setHovered((prev) => !prev);
+  }
+
+  function OpenHandler() {
+    return setOpened((prev) => !prev);
+  }
+
+  function Condition(condition, then, otherwise) {
+    return condition ? then : otherwise;
+  }
+
   return (
-    <div className="select">
-      <div className="select__input body select__input--open">
-        <span className="body-font">Select City</span>
-        <img className="select__bottomarrow" alt="" />
+    <div className="select" onMouseOver={HoverHandler} onMouseOut={HoverHandler} onFocus={HoverHandler} onBlur={HoverHandler}>
+      <div className={`select__input body ${Condition(opened, 'select__input--open', Condition(hovered, 'select__input--hover', ''))}`}>
+        <span className="select__value body-font">{value}</span>
+        <button className="select__arrow" type="button" onClick={OpenHandler} onKeyDown={OpenHandler}>
+          <span role="navigation" className={`${opened ? 'select__toparrow' : 'select__bottomarrow'}`} />
+        </button>
       </div>
 
-      <div className="select__dropdown select__dropdown--open">
+      <div className={`select__dropdown ${opened ? 'select__dropdown--open' : ''}`}>
         <Scrollpicker />
       </div>
     </div>
